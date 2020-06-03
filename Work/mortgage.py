a26342b8e6df32069cea3a17b4a19c989d0b4cb6
Carlos Alberto Ibarra Rosales <cair@MacBook-Pro-de-Carlos.local>
2020-06-03 15:46:38 -0700

    pushing bounce, mortgae and pcost exercise

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,28 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 1
+extra_payment = 1000
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+
+while principal > 0 and principal > payment:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+    if(months >= extra_payment_start_month and months <= extra_payment_end_month):
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+
+    print(f'{months}, {total_paid:0.2f}, {principal:0.2f}')
+    months += 1
+
+payment = principal
+principal = principal - payment
+total_paid = total_paid + payment
+print(f'{months}, {total_paid:0.2f}, {principal:0.2f}')
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,29 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
+def portfolio(filename):
+    total_cost = 0
+
+    f = open(filename)
+    lines = csv.reader(f)
+    headers = next(lines)
+
+    for fields in lines:
+        try:
+            total_cost = total_cost + (int(fields[1]) * float(fields[2]))
+        except ValueError as e:
+            print("couldn't parse", fields)
+
+    return total_cost
+
+#Reading from the command line
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio(filename)
+print('Total cost:', cost)
